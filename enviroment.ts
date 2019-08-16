b6185853b29086e665c146e9ceb1f29c69b8ffdb
jhondielASC6 <jhondiel1234@gmail.com>
2019-08-16 10:18:53 -0400

    mapapp

---
+++ enviroment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+  
+    mapbox: {
+      accessToken: 'pk.eyJ1Ijoiamhvbm55amF5MjMiLCJhIjoiY2p6YWNlamZnMDBkODNpcDd2cXJtcWU2bSJ9.tFQhbAx-l_Sh6OS2CqLpLA'
+    }
+  }
